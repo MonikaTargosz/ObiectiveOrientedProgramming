4dfefc0166d6dcea2150344644315f3d459c1750
Monika Targosz <37025393+MonikaTargosz@users.noreply.github.com>
2020-09-28 15:49:28 +0200

    Version 2
    Wrote the necessary functions for Basic ATM.

--- PrototypeATM/BaseATM.cpp
+++ PrototypeATM/BaseATM.cpp
@@ -1,10 +1,45 @@
+//BasicATM: can only withdraw cash and verify pin
+
+//b) It should only know the current account balance and information whether the ATM is blocked.
+//d) After withdrawing cash, the account balance must be updated, and the ATM must be blocked again.
+//e) You cannot withdraw more than PLN 1,500 from a basic ATM.
+
 #include "BaseATM.h"
 
-BaseATM::BaseATM(void)
-{
+unsigned int BaseATM::CorrectlyPin = 9876;
+unsigned int BaseATM::AccountBalance = 10000;
+
+BaseATM::BaseATM(void){
 }
 
-BaseATM::~BaseATM(void)
-{
+BaseATM::~BaseATM(void){
+}
+
+bool BaseATM::shouldVerifyPin(unsigned int TypedPin){
+	if (CorrectlyPin == TypedPin) {
+		locked = false; //should unblocked ATM after Pin is correctly
+		return true;
+	}
+	else {
+		locked = true; //should blocked ATM after Pin is uncorrectly
+		return false;
+	}
+}
+
+bool BaseATM::shouldWithdrawCashWhenCorrectlyEnteredThePin(unsigned int ValueCash){
+	if (locked == true)
+		return false;
+	else {
+		if (ValueCash > MaxValueWithdrawal || ValueCash > AccountBalance) {
+			locked == true;
+			return false;
+		}
+		else {
+			locked = true;
+			AccountBalance -= ValueCash; //should update the account balance after withdrawing cash
+			return true;
+
+		}
+	}
 }
 
--- PrototypeATM/BaseATM.h
+++ PrototypeATM/BaseATM.h
@@ -2,8 +2,17 @@
 
 class BaseATM
 {
-
+private:
+	static unsigned int CorrectlyPin;
+protected:
+	static unsigned int AccountBalance;
+	unsigned int MaxValueWithdrawal;
+	bool locked;
 public:
 	BaseATM(void);
 	~BaseATM(void);
+
+	bool shouldVerifyPin(unsigned int TypedPin);
+	bool shouldWithdrawCashWhenCorrectlyEnteredThePin(unsigned int ValueCash);
+
 };
