973f5a1a94d0622fd4ed3db18a4b928df2460145
Monika Targosz <37025393+MonikaTargosz@users.noreply.github.com>
2020-10-08 21:35:37 +0200

    Add files via upload
    Implementation of the class of the pointer list and standard collection, and comparison of their performance (dynamic computer memory management).

---
+++ IndicatorListAndStandardCollection/IndicatorListAndStandardCollection/IndicatorListAndStandardCollection.cpp
@@ -0,0 +1,12 @@
+﻿// IndicatorListAndStandardCollection.cpp : Implementation of the class of the pointer list and standard collection, 
+//and comparison of their performance (dynamic computer memory management).
+
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
